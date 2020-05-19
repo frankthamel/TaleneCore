c27e8cc263f0515ff314cf001485926fc66ac18a
Frank <frank.emmanuvel@gmail.com>
2020-05-19 10:37:26 +0530

    Change the podspec.

--- Example/TaleneCore/ViewController.swift
+++ Example/TaleneCore/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import TaleneCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       // let core = TaleneCoreApp()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Source/TaleneCoreApp.swift
@@ -0,0 +1,14 @@
+//
+//  TaleneCoreApp.swift
+//  Pods-TaleneCore_Example
+//
+//  Created by Frank Emmanuel on 5/19/20.
+//
+
+import Foundation
+
+public struct TaleneCoreApp {
+    public init() {
+
+    }
+}
