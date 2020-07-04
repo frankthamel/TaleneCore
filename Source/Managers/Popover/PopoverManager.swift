a210795398cfee068be3e3625bd325d174c923d5
Frank <frank.emmanuvel@gmail.com>
2020-07-04 12:53:00 +0530

    Updated popover

--- Source/Managers/Popover/PopoverManager.swift
+++ Source/Managers/Popover/PopoverManager.swift
@@ -13,6 +13,8 @@ public typealias TCPopoverOption = PopoverOption
 public protocol PopUp {
     func show(view: UIView, atPoint point: CGPoint, options: [TCPopoverOption]?)
     func show(view: UIView, atPoint point: CGPoint, options: [TCPopoverOption]?, showHandler: (() -> ())?, dismissHandler: (() -> ())?)
+    func show(view: UIView, fromFrame frame: UIView, options: [TCPopoverOption]?)
+    func show(view: UIView, fromFrame frame: UIView, options: [TCPopoverOption]?, showHandler: (() -> ())?, dismissHandler: (() -> ())?)
 }
 
 class PopoverManager: PopUp {
@@ -25,5 +27,15 @@ class PopoverManager: PopUp {
     func show(view: UIView, atPoint point: CGPoint, options: [TCPopoverOption]?, showHandler: (() -> ())?, dismissHandler: (() -> ())?) {
         let popover = Popover(options: options, showHandler: showHandler, dismissHandler: dismissHandler)
         popover.show(view, point: point)
+    }
+
+    func show(view: UIView, fromFrame frame: UIView, options: [TCPopoverOption]?) {
+        let popover = Popover(options: options, showHandler: nil, dismissHandler: nil)
+        popover.show(view, fromView: frame)
+    }
+
+    func show(view: UIView, fromFrame frame: UIView, options: [TCPopoverOption]?, showHandler: (() -> ())?, dismissHandler: (() -> ())?) {
+        let popover = Popover(options: options, showHandler: showHandler, dismissHandler: dismissHandler)
+        popover.show(view, fromView: frame)
     }
 }
