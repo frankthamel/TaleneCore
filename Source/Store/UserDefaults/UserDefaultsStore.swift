699185c95547ba032753455b1dba9f35f4b47723
Frank <frank.emmanuvel@gmail.com>
2020-08-14 08:50:41 +0530

    Helper functions aded to userdefaults

--- Source/Store/UserDefaults/UserDefaultsStore.swift
+++ Source/Store/UserDefaults/UserDefaultsStore.swift
@@ -10,7 +10,9 @@ import Foundation
 
 public protocol UserDefaultsStore {
     func save<T>(value: T, forKey key: String)
+    func saveObject<T: Codable>(value: T, forKey key: String) -> Bool
     func getValue<T>(forKey key: String) -> T?
+    func getObject<T: Codable>(forKey key: String) -> T?
 }
 
 struct UserDefaultsStoreProvider: UserDefaultsStore {
@@ -19,8 +21,28 @@ struct UserDefaultsStoreProvider: UserDefaultsStore {
         UserDefaults.standard.set(value, forKey: key)
     }
 
+    func saveObject<T: Codable>(value: T, forKey key: String) -> Bool {
+        let encodedData = try? TCEncodeDecode.encode(object: value)
+        guard let data = encodedData else {
+            return false
+        }
+
+        UserDefaults.standard.set(data, forKey: key)
+        return true
+    }
+
     func getValue<T>(forKey key: String) -> T? {
         return UserDefaults.standard.object(forKey: key) as? T
+    }
+
+    func getObject<T: Codable>(forKey key: String) -> T? {
+        let dataString: String? = getValue(forKey: key)
+        guard let data = dataString  else {
+            return nil
+        }
+        
+        let object: T? = try? TCEncodeDecode.decode(string: data)
+        return object
     }
 
 }
